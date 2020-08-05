b943b3a7491a5befb236ebe8bafaf9e6801c96ec
Gianmarco Doro <gianmarco.doro@gmail.com>
2020-08-05 23:13:24 +0200

    Add an in-game terminal

--- BullCow/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCow/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
+    PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have Lost!"));
+    }
 }
